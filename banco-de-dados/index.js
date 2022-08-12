9e79795536da69a9aa5e651e5ae664950ebf27b5
devjs.fabiano <devjs.fabiano@gmail.com>
2022-08-12 16:27:18 -0300

    bd alter

--- banco-de-dados/index.js
+++ banco-de-dados/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const instancia = new sequelize(
     'petshop',
     'root',
-    'Fijpac0705!!',
+    '******',
     {
         host: '127.0.0.1',
         dialect: 'mysql'
